72522964d997ea53ceaf80f9de114119f82ce0bb
vignesh g <vignesh.g@aspiredigital.com>
2021-11-20 16:54:48 +0530

    grantt chart page created

--- src/container/home/Home.tsx
+++ src/container/home/Home.tsx
@@ -2,14 +2,15 @@ import React from "react";
 import { Route, Switch } from "react-router";
 import GroundUpDrawer from "../../components/common/drawer/Drawer";
 import GroundUpAppBar from "../../components/common/header/GroundUpAppBar";
-import { HOME_BOOKING } from "../../constants/ContextPaths";
+import { HOME_BOOKING, HOME_GRANTT_CHART } from "../../constants/ContextPaths";
 import BookingView from "../../components/booking-view/BookingView";
+import GranttChartView from "../../components/grant-chart-view/GranttChartView";
 
 interface HomeProps {
     children?: any;
-  }
+}
 
-const Home: React.FC<HomeProps>  = (props: HomeProps) => {
+const Home: React.FC<HomeProps> = (props: HomeProps) => {
     const [open, setOpen] = React.useState(false);
     const [headerTab, setHeaderTab] = React.useState(0);
 
@@ -17,20 +18,23 @@ const Home: React.FC<HomeProps>  = (props: HomeProps) => {
         setOpen(!open);
     };
     const drawerItems = [
-        {text:'Crane Booking', id: HOME_BOOKING, notifications:20}, 
-        {text:'Grantt Chart', id: '/home/chart'}, 
-        {text:'Calendar', id:'/home/calendar'}
+        { text: 'Crane Booking', id: HOME_BOOKING, notifications: 20 },
+        { text: 'Grantt Chart', id: HOME_GRANTT_CHART },
+        { text: 'Calendar', id: '/home/calendar' }
     ]
     return (
         <>
-            <GroundUpAppBar headerTab={headerTab} setHeaderTab={setHeaderTab} handleDrawerToggle={handleDrawerToggle}/>
+            <GroundUpAppBar headerTab={headerTab} setHeaderTab={setHeaderTab} handleDrawerToggle={handleDrawerToggle} />
             <GroundUpDrawer items={drawerItems} mobileOpen={open} handleDrawerToggle={handleDrawerToggle}>
                 <Switch>
                     <Route path={HOME_BOOKING}>
-                        <BookingView/>
+                        <BookingView />
+                    </Route>
+                    <Route path={HOME_GRANTT_CHART}>
+                        <GranttChartView />
                     </Route>
                 </Switch>
-            </GroundUpDrawer> 
+            </GroundUpDrawer>
         </>
     );
 }
