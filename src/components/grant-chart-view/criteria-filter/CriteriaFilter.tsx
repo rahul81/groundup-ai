edcccb77d8069b5d1d9a0459c3a67456503b56ba
vignesh g <vignesh.g@aspiredigital.com>
2021-11-20 17:02:54 +0530

    criterial filter updated with common select and date components

--- src/components/grant-chart-view/criteria-filter/CriteriaFilter.tsx
+++ src/components/grant-chart-view/criteria-filter/CriteriaFilter.tsx
@@ -1,40 +1,42 @@
 import { SelectChangeEvent } from '@mui/material';
 import { Box } from '@mui/system'
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import { GDatePicker } from '../../common/date-picker/GDatePicker';
+import { GSelect, GSelectOption } from '../../common/select/Gselect';
 
 export default function CriteriaFilter() {
     const [crane, setCrane] = useState('')
     const [zone, setZone] = useState('');
+    const [date, setDate] = useState(new Date());
+    const cranes: GSelectOption[] = [
+        { key: "crane1", value: "Crane1" },
+        { key: "crane2", value: "Crane2" },
+        { key: "crane3", value: "Crane3" }
+    ];
+    const zones: GSelectOption[] = [
+        { key: "zone1", value: "Zone1" },
+        { key: "zone2", value: "Zone2" },
+        { key: "zone3", value: "Zone3" }
+    ];
 
-    const handleChangeCrane = (event: SelectChangeEvent) => {
-        setCrane(event.target.value);
+    const handleChangeCrane = (value: string) => {
+        setCrane(value);
     };
-    const handleChangeZone = (event: SelectChangeEvent) => {
-        setZone(event.target.value);
+    const handleChangeZone = (value: string) => {
+        setZone(value);
     };
 
     return (
         <>
             <Box sx={{ display: 'flex' }} mb={1}>
                 <Box className="dropdown" mr={1}>
-                    <button className="btn btn-secondary dropdown-toggle dropdown-button" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-                        Select Crane
-                    </button>
-                    <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                        <li><a className="dropdown-item" href="#">Crane 1</a></li>
-                        <li><a className="dropdown-item" href="#">Crane 2</a></li>
-                        <li><a className="dropdown-item" href="#">Crane 3</a></li>
-                    </ul>
+                    <GDatePicker date={date} onChange={setDate} />
                 </Box>
                 <Box className="dropdown" mr={1}>
-                    <button className="btn btn-secondary dropdown-toggle dropdown-button" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-                        Select Zone
-                    </button>
-                    <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                        <li><a className="dropdown-item" href="#">Zone 1</a></li>
-                        <li><a className="dropdown-item" href="#">Zone 2</a></li>
-                        <li><a className="dropdown-item" href="#">Zone 3</a></li>
-                    </ul>
+                    <GSelect id="Crane" placeholder="Select Crane" options={cranes} onChange={handleChangeCrane} value={crane} />
+                </Box>
+                <Box className="dropdown" mr={1}>
+                    <GSelect id="Crane" placeholder="Select Zone" options={zones} onChange={handleChangeZone} value={zone} />
                 </Box>
             </Box>
         </>
