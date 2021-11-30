338cc38c42f0bba9f164d11105d2666aab14493d
sanket patil <sanket.9006@gmail.com>
2021-11-30 20:52:25 +0530

    Added formik into Reports

--- src/components/admin-view/management/reports/ReportsManagement.tsx
+++ src/components/admin-view/management/reports/ReportsManagement.tsx
@@ -1,11 +1,13 @@
 import { Box, Typography, Divider, Checkbox } from '@mui/material'
 import React from 'react'
+import { useFormik } from 'formik';
 import GButton from '../../../common/button/GButton';
+import GCheckbox from '../../../common/checkobx/GCheckbox';
 import '../../admin-view.scss'
 
 interface Notification{
     text:string;
-    selected:boolean;
+    checkedUnchecked:string;
 }
 
 interface Notifications{
@@ -15,28 +17,38 @@ interface Notifications{
 
 export default function ReportsManagement() {
 
+    const checkedUnchecked: any = {
+        booking_BS: true,
+        booking_BRL: false,
+        crane_CUS: false,
+        crane_CUR: false,
+        crane_IML: false,
+        maintainence_PMR: false,
+        maintainence_ML: true,
+    }
+
     const booking: Notifications = {
         text: "Booking",
         notifications : [
-            {text: "Booking Summary", selected: true},
-            {text: "Booking Record list", selected: false}
+            {text: "Booking Summary", checkedUnchecked: "booking_BS"},
+            {text: "Booking Record list", checkedUnchecked: "booking_BRL"}
         ]
     };
 
     const crane: Notifications = {
         text: "Crane",
         notifications : [
-            {text: "Crane Utilisation Summary", selected: false},
-            {text: "Crane Utilization Records", selected: false},
-            {text: "Indentified Materials List", selected: false}
+            {text: "Crane Utilisation Summary", checkedUnchecked: "crane_CUS"},
+            {text: "Crane Utilization Records", checkedUnchecked: "crane_CUR"},
+            {text: "Indentified Materials List", checkedUnchecked: "crane_IML"}
         ]
     };
 
     const maintainence: Notifications = {
         text: "Maintainance",
         notifications : [
-            {text: "Planned Maintainance Records", selected: true},
-            {text: "Materials lifted", selected: false}
+            {text: "Planned Maintainance Records", checkedUnchecked: "maintainence_PMR"},
+            {text: "Materials lifted", checkedUnchecked: "maintainence_ML"}
         ]
     };
 
@@ -44,24 +56,33 @@ export default function ReportsManagement() {
         booking, crane, maintainence
     ];
 
-    const changeStatus = (item: Notification)=>{
-        item.selected = !item.selected;
-    };
+    const formik = useFormik({
+        initialValues: checkedUnchecked,
+        onSubmit: (data) => {
+            console.log(data);
+        },
+    });
 
     return (
         <Box className="reports-view">
             <Typography className="heading" variant="h5" component="h2">Reports Management</Typography>
             <Divider/>
+            <form onSubmit={formik.handleSubmit}>
             {(allNotifications || []).map((notif) => <Box className="sub-section">
                 {notif && <Typography className="heading" variant="h6" component="div">{notif.text}</Typography>}
                 {(notif.notifications || [] ).map((item, index)=>  
-                    <Box className="item">
-                        <Checkbox className="item-checkbox" aria-label={item.text} checked={item.selected} onClick={(e)=>changeStatus(item)}/>
-                        <Typography variant="body1" >{item.text}</Typography>
+                    <Box className="item">                            
+                        <GCheckbox 
+                        checkedUncheckedState={item.checkedUnchecked} 
+                        formik={formik} 
+                        id={item.checkedUnchecked} 
+                        label={item.text}/>
                     </Box>
                 )}
             </Box>)}
-            <GButton title='Save Changes' className="update-changes" sx={{mt:3}} />
+            <GButton type="submit" title='Save Changes' className="update-changes" sx={{mt:3}} />
+            </form>
+
         </Box>
     )
 }
