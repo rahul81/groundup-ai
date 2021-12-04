abcca05ef62f2ca50e5baff4acf95e5de96b0b8a
sanket patil <sanket.9006@gmail.com>
2021-12-04 11:52:10 +0530

    Add User Dialog

--- src/components/admin-view/management/user/UserManagement.tsx
+++ src/components/admin-view/management/user/UserManagement.tsx
@@ -1,16 +1,22 @@
 import { Box, Typography, Divider } from '@mui/material'
-import React from 'react'
+import { useState } from 'react'
 import { UserManagementColumns, UserManagementRows } from '../../../../mockData/AdminPanel'
 import GButton from '../../../common/button/GButton'
 import GTable from '../../../common/table/GTable'
 import '../../admin-view.scss'
+import AddUser from './add-user/AddUser'
 
 export default function UserManagement() {
+    const [open, setOpen] = useState(false);
+    const handleShowDialog = (status:boolean)=>{
+        setOpen(status);
+    }
     return (
         <Box className="crane-booking-management-view">
             <Typography className="heading" variant="h5" component="h2">User Management</Typography>
             <Divider/>
-            <GButton title='Add User' size='small' style={{ display:'block', margin:'10px 0' }} />
+            <GButton title='Add User' size='small' style={{ display:'block', margin:'10px' }} onClick={()=>setOpen(true)}/>
+            <AddUser open={open} showDialog={handleShowDialog} handleSubmit={()=>{setOpen(false)}}/>
             <GTable rowClicked={(data: any) => {}} rows={UserManagementRows} columns={UserManagementColumns} />
         </Box>
     )
