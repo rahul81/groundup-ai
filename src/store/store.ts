31289c8cb5c049ff145bf1227d3b88dac3879c58
sanket patil <sanket.9006@gmail.com>
2021-12-10 10:29:56 +0530

    added redux-devtools-extension

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,9 +2,10 @@ import { applyMiddleware, createStore } from "redux";
 import reducers from './reducers';
 import thunk from 'redux-thunk';
 import { useDispatch } from "react-redux";
+import {composeWithDevTools} from 'redux-devtools-extension'
 
 export const store = createStore(
   reducers,
   {},
-  applyMiddleware(thunk)
+  composeWithDevTools(applyMiddleware(thunk))
 )
