7f81e39c999f8ba9d88806e08b1dad7dd59cc6fa
ShivPratap21 <shiv.gwl123@gmail.com>
2021-12-13 21:55:04 +0530

    API interation token update

--- src/store/action-creators/adminActionCreators.ts
+++ src/store/action-creators/adminActionCreators.ts
@@ -7,18 +7,9 @@ import { RootState } from '../reducers';
 
 export type AppThunk<R> = ThunkAction<R, RootState, null, Action>;
 
-const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MWFmMzYzYTJlNWU5NDZjZTlmYWNjZmMiLCJpYXQiOjE2MzkwMjAxNjV9.PTbWO3_X7a6HifMX9nPQmflxSFbXgEl3nHn_3fW0bLY"
-
-const config = {
-    headers: {  
-        'Content-Type': 'application/json', 
-        'Authorization': `Bearer ${token}` }
-}
-const bodyParameters = { };
-
 export const getUsers = (): AppThunk<void> =>{
      return async (dispatch: Dispatch<Action>)=> {
-        return await axios.post(GET_USERS, bodyParameters, config)
+        return await axios.post(GET_USERS, {})
         .then(response=>{
             dispatch(getUsersSuccess(response.data.data));
         }).catch(error=>{
--- src/store/store.ts
+++ src/store/store.ts
@@ -2,10 +2,9 @@ import { applyMiddleware, createStore } from "redux";
 import reducers from './reducers';
 import thunk from 'redux-thunk';
 import { useDispatch } from "react-redux";
-import {composeWithDevTools} from 'redux-devtools-extension'
 
 export const store = createStore(
   reducers,
   {},
-  composeWithDevTools(applyMiddleware(thunk))
+  applyMiddleware(thunk)
 )
